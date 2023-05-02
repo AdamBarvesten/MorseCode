913a0f7e69c2baf78d0cadb5a0bd92eb2992319a
adambarvesten <adam.varg@hotmail.com>
2023-05-02 15:00:45 +0200

    Add 2 more sounds for A/B testing for sound

--- app/src/main/java/com/example/morsecode/SoundActivity.java
+++ app/src/main/java/com/example/morsecode/SoundActivity.java
@@ -25,16 +25,33 @@ public class SoundActivity extends AppCompatActivity {
         Button longButton2 = findViewById(R.id.longButton2);
         Button letterButton2 = findViewById(R.id.letterButton2);
 
-        shortButton.setOnClickListener(view -> onBtnShort());
-        longButton.setOnClickListener(view -> onBtnLong());
-        letterButton.setOnClickListener(view -> onBtnLetter());
+        Button shortButton3 = findViewById(R.id.shortButton3);
+        Button longButton3 = findViewById(R.id.longButton3);
+        Button letterButton3 = findViewById(R.id.letterButton3);
 
-        shortButton2.setOnClickListener(view -> onBtnShort2());
-        longButton2.setOnClickListener(view -> onBtnLong2());
-        letterButton2.setOnClickListener(view -> onBtnLetter2());
+        Button shortButton4 = findViewById(R.id.shortButton4);
+        Button longButton4 = findViewById(R.id.longButton4);
+        Button letterButton4 = findViewById(R.id.letterButton4);
+
+        shortButton.setOnClickListener(view -> onBtnShort(1));
+        longButton.setOnClickListener(view -> onBtnLong(1));
+        letterButton.setOnClickListener(view -> onBtnLetter(1));
+
+        shortButton2.setOnClickListener(view -> onBtnShort(2));
+        longButton2.setOnClickListener(view -> onBtnLong(2));
+        letterButton2.setOnClickListener(view -> onBtnLetter(2));
+
+        shortButton3.setOnClickListener(view -> onBtnShort(3));
+        longButton3.setOnClickListener(view -> onBtnLong(3));
+        letterButton3.setOnClickListener(view -> onBtnLetter(3));
+
+        shortButton4.setOnClickListener(view -> onBtnShort(4));
+        longButton4.setOnClickListener(view -> onBtnLong(4));
+        letterButton4.setOnClickListener(view -> onBtnLetter(4));
+
     }
 
-    private void playLetter( Character c){
+    private void playLetter( Character c, int soundIdx){
         String morse = morseCode.getMorse(c);
 
         new Thread(() -> {
@@ -42,9 +59,9 @@ public class SoundActivity extends AppCompatActivity {
                 try {
                     for(int i = 0; i < morse.length(); i++){
                         if(morse.charAt(i) == '.'){
-                            playShortTone();
+                            playShortTone(soundIdx);
                         }else{
-                            playLongTone();
+                            playLongTone(soundIdx);
                         }
                         toneGenerator.wait(100+500);
                     }
@@ -55,52 +72,35 @@ public class SoundActivity extends AppCompatActivity {
         }).start();
     }
 
-    private void playLetter2( Character c){
-        String morse = morseCode.getMorse(c);
+    private void playSound(int len){ toneGenerator.startTone(ToneGenerator.TONE_DTMF_8,len); }
 
-        new Thread(() -> {
-            synchronized (toneGenerator){
-                try {
-                    for(int i = 0; i < morse.length(); i++){
-                        if(morse.charAt(i) == '.'){
-                            playShortTone2();
-                        }else{
-                            playLongTone2();
-                        }
-                        toneGenerator.wait(100+500);
-                    }
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }).start();
+    private void playSound2(int len){ toneGenerator.startTone(ToneGenerator.TONE_PROP_NACK, len); }
+
+    private void playSound3(int len){ toneGenerator.startTone(ToneGenerator.TONE_SUP_DIAL, len); }
+
+    private void playSound4(int len){ toneGenerator.startTone(ToneGenerator.TONE_CDMA_DIAL_TONE_LITE, len); }
+
+    private void playLongTone(int i){
+        switch (i){
+            case 1 : playSound(500); break;
+            case 2 : playSound2(500); break;
+            case 3 : playSound3(500); break;
+            case 4 : playSound4(500); break;
+        }
+     }
+    private void playShortTone(int i){
+        switch (i){
+            case 1 : playSound(100); break;
+            case 2 : playSound2(100); break;
+            case 3 : playSound3(100); break;
+            case 4 : playSound4(100); break;
+        }
     }
 
-    private void playSound(int len){ toneGenerator.startTone(ToneGenerator.TONE_SUP_DIAL,len); }
-
-    private void playSound2(int len){ toneGenerator.startTone(ToneGenerator.TONE_CDMA_DIAL_TONE_LITE,len); }
-
-    private void playLongTone(){ playSound(500); }
-    private void playShortTone(){ playSound(100); }
-
-    private void playLongTone2(){ playSound2(500); }
-    private void playShortTone2(){ playSound2(100); }
-
-    private void onBtnShort(){
-        playShortTone();
+    private void onBtnShort(int i){
+        playShortTone(i);
     }
-    private void onBtnLong(){
-        playLongTone();
-    }
+    private void onBtnLong(int i){ playLongTone(i);}
 
-    private void onBtnShort2() { playShortTone2();}
-    private void onBtnLong2() { playLongTone2(); }
-
-    private void onBtnLetter(){ playLetter('x'); }
-
-    private void onBtnLetter2() { playLetter2('x');}
-
-
-
-
+    private void onBtnLetter(int i){ playLetter('x', i); }
 }
