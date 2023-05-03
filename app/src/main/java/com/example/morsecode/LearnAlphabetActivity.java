e0e4f5168f3924591c9d3b99a973dffda122c9bc
BardNilz <ma6768ba-s@student.lu.se>
2023-05-03 15:59:34 +0200

    Made the "a"-Button to be clickable

--- app/src/main/java/com/example/morsecode/LearnAlphabetActivity.java
+++ app/src/main/java/com/example/morsecode/LearnAlphabetActivity.java
@@ -2,13 +2,36 @@ package com.example.morsecode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class LearnAlphabetActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn_alphabet);
+
+        Button aButton = findViewById(R.id.button_a);
+        final Dialog dialog = new Dialog(this, android.R.style.Theme_Translucent_NoTitleBar_Fullscreen);
+        dialog.setContentView(R.layout.dialog_layout);
+        ImageView imageView = dialog.findViewById(R.id.a_morse_img);
+
+        aButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setImageResource(R.drawable.amorse);
+                dialog.show();
+            }
+        });
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
     }
 }
