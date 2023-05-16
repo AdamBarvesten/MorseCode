4836cc77c12cebff30a22cc2bdd93f00f8905547
BardNilz <ma6768ba-s@student.lu.se>
2023-05-16 13:30:11 +0200

    alphabet merge

--- app/src/main/java/com/example/morsecode/LearnAlphabetActivity.java
+++ app/src/main/java/com/example/morsecode/LearnAlphabetActivity.java
@@ -2,12 +2,20 @@ package com.example.morsecode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
+import android.media.ToneGenerator;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class LearnAlphabetActivity extends AppCompatActivity {
+    private final MorseCode morseCode = new MorseCode();
+    Vibrator vibrator;
+    private final ToneGenerator toneGenerator =  new ToneGenerator(AudioManager.STREAM_MUSIC, ToneGenerator.TONE_SUP_DIAL);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,12 +25,71 @@ public class LearnAlphabetActivity extends AppCompatActivity {
         final Dialog dialog = new Dialog(this, android.R.style.Theme_Translucent_NoTitleBar_Fullscreen);
         dialog.setContentView(R.layout.dialog_layout);
         ImageView imageView = dialog.findViewById(R.id.a_morse_img);
+        Button playButton = dialog.findViewById(R.id.play_button);
 
-        aButton.setOnClickListener(v -> {
-            imageView.setImageResource(R.drawable.amorse);
-            dialog.show();
+        vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE); // Initialize the vibrator
+
+        aButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setImageResource(R.drawable.amorse);
+                dialog.show();
+            }
         });
 
-        imageView.setOnClickListener(v -> dialog.dismiss());
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Shake event detected", Toast.LENGTH_SHORT).show();
+
+                playLetter('A', 0); // Provide the appropriate arguments to playLetter() method
+            }
+        });
+    }
+
+    private void playLetter(Character c, int soundIdx) {
+        String morse = morseCode.getMorse(c);
+
+        if (morse != null) {
+            new Thread(() -> {
+                synchronized (toneGenerator) {
+                    try {
+                        for (int i = 0; i < morse.length(); i++) {
+                            if (morse.charAt(i) == '.') {
+                                playShortTone(soundIdx);
+                                playShortVibe();
+                                Thread.sleep(1000);  // Pause for 1 second between short tones/vibrations
+                            } else if (morse.charAt(i) == '-') {
+                                playLongTone(soundIdx);
+                                playLongVibe();
+                                Thread.sleep(2000);  // Pause for 2 seconds between long tones/vibrations
+                            }
+                            Thread.sleep(500);  // Pause for 0.5 seconds between each dot/dash
+                        }
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }).start();
+        }
+    }
+
+
+
+
+    private void playShortVibe() {
+        vibrator.vibrate(100);
+    }
+
+    private void playLongVibe() {
+        vibrator.vibrate(500);
+    }
+
+    private void playLongTone(int i) {
+        toneGenerator.startTone(ToneGenerator.TONE_SUP_DIAL, 500);
+    }
+
+    private void playShortTone(int i) {
+        toneGenerator.startTone(ToneGenerator.TONE_SUP_DIAL, 100);
     }
 }
